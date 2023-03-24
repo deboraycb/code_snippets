7eb6414de56963fcc237d43c9aa3e7b1e8b5aa43
deboraycb <3833873+deboraycb@users.noreply.github.com>
2023-03-24 14:31:52 +0000

    python-argparse-style passing of command line arguments to R

--- RcommandLineArgs.R
+++ RcommandLineArgs.R
@@ -1,12 +1,10 @@
 #!/usr/bin/env Rscript
 library("optparse")
  
-option_list = list(
-  make_option(c("-f", "--file"), type="character", default=NULL, 
+option_list = list(make_option(c("-f", "--file"), type="character", default=NULL, 
               help="dataset file name", metavar="character"),
-    make_option(c("-o", "--out"), type="character", default="out.txt", 
-              help="output file name [default= %default]", metavar="character")
-); 
+                   make_option(c("-o", "--out"), type="character", default="out.txt", 
+              help="output file name [default= %default]", metavar="character")) 
  
-opt_parser = OptionParser(option_list=option_list);
-opt = parse_args(opt_parser);
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
