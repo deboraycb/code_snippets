5beac10302d0d5b68fc4945d1879e48c25b3229e
deboraycb <3833873+deboraycb@users.noreply.github.com>
2023-03-24 14:37:17 +0000

    python-argparse-style passing of command line arguments to R

--- RcommandLineArgs.R
+++ RcommandLineArgs.R
@@ -8,3 +8,8 @@ option_list = list(make_option(c("-f", "--file"), type="character", default=NULL
  
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
+
+if (is.null(opt$file)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
