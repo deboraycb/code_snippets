f1c3905da052a2bd17596b07e4484dd5580ed2d4
deboraycb <3833873+deboraycb@users.noreply.github.com>
2023-06-26 11:25:26 +0100

    print help

--- python_argparse.py
+++ python_argparse.py
@@ -1,6 +1,9 @@
 import argparse
-parser = argparse.ArgumentParser()
+import sys
+
+parser = argparse.ArgumentParser(description="this is what this program does")
 parser.add_argument("-i", "--input", help="input file name")
 parser.add_argument("-o", "--output", help="output file name",
 					default="out")
-args = parser.parse_args()
+
+args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
